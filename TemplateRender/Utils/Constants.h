446ff7fbdfc6a1d4137ecd60593f1d8ed44b2f2e
Yuriy Lisovskiy <yuriy@sol-ra.com>
2017-11-28 19:36:07 +0200

    added regex for 'if' statement

--- TemplateRender/Utils/Constants.h
+++ TemplateRender/Utils/Constants.h
@@ -4,15 +4,15 @@
 namespace CONSTANT
 {
 	const std::string FOR_REGEX("\\s*for\\s*\\(\\s*(auto|size_t|int)\\s*[a-z]{1,}\\s*=\\s*\\d{1,}\\s*;\\s*[a-z]{1,}\\s*(<|>|==|!=)\\s*\\d{1,}\\s*;\\s*[a-z]{1,}((\\+\\+)|(\\-\\-))\\s*\\)");
-	//const std::string FOREACH_REGEX = "\\s*for\\s*\\(\\s*auto\\s*[a-z]{1,}\\s*\\:\\s*[a-z]{1,}\\s*\\)";
-	const std::string IF_REGEX("\\s*if\\s*\\(\\s*[a-z]{1,}\\s*< |\\s*> \\s*[a-z]{1,}\\s*\\)");
+	const std::string IF_REGEX("\\s*if\\s*\\(\\s*((\\w+)|(\\w+\\s*(<|>|==|!=|<=|>=)\\s*\\w+))\\s*\\)");
 
 	const std::string VAR_REGEX("\\{\\{\\s*\\w*\\s*\\}\\}");
 
+	const std::string BEGIN_IF_REGEX("\\{\\s*%" + IF_REGEX + "\\s*%\\s*\\}");
 	const std::string BEGIN_FOR_REGEX("\\{\\s*%" + FOR_REGEX + "\\s*%\\s*\\}");
 	const std::string BEGIN_COMMENT_REGEX("\\{\\s*%\\s*comment\\s*\".*\"\\s*%\\s*\\}");
 
-	const std::string BEGIN_TAG_REGEX("(" + BEGIN_FOR_REGEX + ")|(" + BEGIN_COMMENT_REGEX + ")");
+	const std::string BEGIN_TAG_REGEX("(" + BEGIN_FOR_REGEX + ")|(" + BEGIN_COMMENT_REGEX + ")|(" + BEGIN_IF_REGEX + ")");
 	const std::string END_TAG_REGEX("\\{\\s*%\\s*end(for|if|comment)\\s*%\\s*\\}");
 
 	const std::string TEMPLATE_DIR("Tests/");
